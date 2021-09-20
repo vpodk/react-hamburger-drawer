d1026f0fbf2d0382619c86eccfe5611e25d9cdee
Valentin Podkamennyi <vpodkamenyi@gmail.com>
2021-09-19 17:19:23 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+
+# react-hamburger-drawer
+Simple lightweight react hamburger navigation drawer.
+
+## Usage:
+
+```js
+import {HamburgerDrawer} from 'react-hamburger-drawer';
+```
+
+```html
+<div className="App">
+  <header>
+    <HamburgerDrawer>
+      <!-- Your navigation drawer content here: -->
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+        <li><a href="/privacy-policy">Privacy Policy</a></li>
+      </ul>
+      </HamburgerDrawer>
+      <h1>Your Application Header</h1>
+      ...
+  </header>
+  ...
+</div>
+```
---
+++ index.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useRef } from "react";
+import "./index.css";
+
+export const HamburgerDrawer = ({ children }) => {
+  const checkbox = useRef(null);
+
+  const handleEscKey = (event) => {
+    // Closes the navigation drawer by pressing Esc key.
+    if ("Escape" === event.key) {
+      checkbox.current.checked = false;
+    }
+  };
+
+  const handleKeyup = (event) => {
+    if ("Enter" === event.key) {
+      checkbox.current.checked = !checkbox.current.checked;
+    }
+  };
+
+  const handleClick = () => {
+    document.documentElement.classList.toggle("HamburgerDrawer-open");
+  };
+
+  useEffect(() => {
+    document.addEventListener("keyup", handleEscKey);
+    return () => document.removeEventListener("keyup", handleEscKey);
+  }, []);
+
+  return (
+    <nav className="HamburgerDrawer" aria-label="Main menu">
+      <input
+        ref={checkbox}
+        type="checkbox"
+        id="HamburgerDrawer-toggle"
+        aria-label="Toggle menu"
+        onKeyUp={handleKeyup}
+        onClick={handleClick}
+      />
+      <label
+        htmlFor="HamburgerDrawer-toggle"
+        tabIndex="-1"
+        aria-hidden="true"
+        hidden
+      >
+        Close menu
+      </label>
+      <div
+        className="HamburgerDrawer-icon"
+        role="button"
+        aria-label="Toggle menu"
+      >
+        <span></span>
+      </div>
+      <div className="HamburgerDrawer-panel">{children}</div>
+    </nav>
+  );
+};
