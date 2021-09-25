475e257a8e11b15e89e26a9a5d761161c5b15234
Valentin Podkamennyi <vpodkamenyi@gmail.com>
2021-09-24 21:35:33 -0700

    Added index.d.ts file

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+import React from "react";
+
+export interface HamburgerDrawerProps {
+  children: React.ReactNode;
+}
+
+declare const HamburgerDrawer: React.FunctionComponent<HamburgerDrawerProps>;
+
+export default HamburgerDrawer;
